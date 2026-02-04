e70aacc9e76e84c32e895a77d900969c58b08d64
Rudraksh Kushwaha <113119934+rudrakushwaha@users.noreply.github.com>
2026-02-04 23:30:15 +0530

    Time: 23 ms (31.54%) | Memory: 53.9 MB (7.54%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,19 +1,40 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        
-        int count = 0;
-        int sum = 0;
 
-        for( int i = 0; i < nums.length ; i++){
-            sum = 0;
-            for(int j = i; j < nums.length ; j++){
+        //================APPROACH 2=================
+         int count = 0;
+        HashMap<Long, Integer> map = new HashMap<>();
+        long sum = 0;
+        long remaining = 0;
+        map.put(0L,1);
 
-                sum += nums[j];
-                if(sum == goal) count++;
-                else if( sum > goal) break;
+        for( int i = 0 ; i < nums.length ; i++){
+
+            sum += nums[i];
+            remaining = sum - goal;
+
+            if( map.containsKey(remaining)){
+               count += map.get(remaining);
             }
-        }
+             map.put(sum, map.getOrDefault(sum, 0) + 1);
 
-        return count;
+        }
+		return count;
+        
+        //==================APPROACH 1==================
+        // int count = 0;
+        // int sum = 0;
+
+        // for( int i = 0; i < nums.length ; i++){
+        //     sum = 0;
+        //     for(int j = i; j < nums.length ; j++){
+
+        //         sum += nums[j];
+        //         if(sum == goal) count++;
+        //         else if( sum > goal) break;
+        //     }
+        // }
+
+        // return count;
     }
 }
