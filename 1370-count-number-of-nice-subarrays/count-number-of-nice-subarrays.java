f1e0783be95d1733f1a6952098b83610d45699ed
Rudraksh Kushwaha <113119934+rudrakushwaha@users.noreply.github.com>
2026-02-09 23:29:52 +0530

    Time: 12 ms (70.80%) | Memory: 59.9 MB (23.94%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        
+        //==============APPPROACH 2==================
+
+        // for( int  num: nums){
+
+        //     if(num % 2 != 0) num = 1;
+        //     else num = 0;
+        // }
+        for( int i = 0 ; i < nums.length ; i++){
+
+            if( nums[i] % 2 != 0) nums[i] = 1;
+            else nums[i] = 0;
+        }
+
+        return subArrCount(nums, k) - subArrCount(nums, k - 1);
+
+
+        //==============APPROACH 1 (BRUTE)==============
+        // int oddCount = 0;
+        // int count = 0;
+
+        // for( int i = 0; i < nums.length; i++){
+        //     oddCount = 0;
+        //     for(int j = i; j < nums.length; j++){
+        //         if( nums[j] % 2 != 0) oddCount++;
+
+        //         if( oddCount == k) {
+        //             count++;
+        //         }
+        //     }
+        // }
+
+        // return count;
+    }
+
+    public int subArrCount(int[] nums , int goal) {
+
+        if ( goal < 0) return 0;
+
+        int l = 0, r = 0;
+        int count = 0;
+        int n = nums.length;
+        int sum = 0;
+
+        while( r < n){
+
+            sum += nums[r];
+
+            while( sum > goal){
+                sum -= nums[l];
+                l += 1;
+            }
+            count += (r - l + 1);
+
+            r++;
+
+        }
+
+        return count;
+    }
+}
