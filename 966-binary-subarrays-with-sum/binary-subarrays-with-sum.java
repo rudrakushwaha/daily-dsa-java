6db28e896865a11dd39105ad8c315f1b36675683
Rudraksh Kushwaha <113119934+rudrakushwaha@users.noreply.github.com>
2026-02-09 21:45:08 +0530

    Time: 1 ms (99.83%) | Memory: 51.1 MB (37.70%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,25 +1,29 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
 
-        //================APPROACH 2=================
-         int count = 0;
-        HashMap<Long, Integer> map = new HashMap<>();
-        long sum = 0;
-        long remaining = 0;
-        map.put(0L,1);
+        //================APPROACH 3======================
+        //algo which count sum <= k 
+        return subArrCount(nums, goal) - subArrCount(nums, goal - 1);
 
-        for( int i = 0 ; i < nums.length ; i++){
+        //================APPROACH 2 (similar to longest sub with sum k and count subarray with sum k)=================
+        //  int count = 0;
+        // HashMap<Long, Integer> map = new HashMap<>();
+        // long sum = 0;
+        // long remaining = 0;
+        // map.put(0L,1);
 
-            sum += nums[i];
-            remaining = sum - goal;
+        // for( int i = 0 ; i < nums.length ; i++){
 
-            if( map.containsKey(remaining)){
-               count += map.get(remaining);
-            }
-             map.put(sum, map.getOrDefault(sum, 0) + 1);
+        //     sum += nums[i];
+        //     remaining = sum - goal;
 
-        }
-		return count;
+        //     if( map.containsKey(remaining)){
+        //        count += map.get(remaining);
+        //     }
+        //      map.put(sum, map.getOrDefault(sum, 0) + 1);
+
+        // }
+		// return count;
         
         //==================APPROACH 1==================
         // int count = 0;
@@ -36,5 +40,31 @@ class Solution {
         // }
 
         // return count;
+    }
+
+    public int subArrCount(int[] nums , int goal) {
+
+        if ( goal < 0) return 0;
+
+        int l = 0, r = 0;
+        int count = 0;
+        int n = nums.length;
+        int sum = 0;
+
+        while( r < n){
+
+            sum += nums[r];
+
+            while( sum > goal){
+                sum -= nums[l];
+                l += 1;
+            }
+            count += (r - l + 1);
+
+            r++;
+
+        }
+
+        return count;
     }
 }
