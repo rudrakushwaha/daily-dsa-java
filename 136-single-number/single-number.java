a78d7ea6fd7826a7b8b3513564da541d3a4ba29b
Rudraksh Kushwaha <113119934+rudrakushwaha@users.noreply.github.com>
2026-02-20 23:55:27 +0530

    Time: 1 ms (99.89%) | Memory: 47 MB (64.68%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,27 +1,29 @@
 class Solution {
     public int singleNumber(int[] nums) {
+
+        ///===========APPROACH 3 (BETTER APPROACH)==============
         
 
         //===============APPROACH 2 brute force===============
-        int count = 0;
-        for(int i  = 0 ; i < nums.length ; i++){
-            int num = nums[i];
-            count = 0;
-            for(int j = 0 ; j < nums.length ; j++){
+        // int count = 0;
+        // for(int i  = 0 ; i < nums.length ; i++){
+        //     int num = nums[i];
+        //     count = 0;
+        //     for(int j = 0 ; j < nums.length ; j++){
 
-                if(nums[j] == num) count++;
-            }
+        //         if(nums[j] == num) count++;
+        //     }
 
-            if(count == 1) return nums[i];
-        }
-        return 0;
-        //===============APPROACH 1=================
-        // int xor = 0;
-        // int n = nums.length;
-        // for(int i = 0 ; i < n ; i++){
-        //     xor = xor ^ nums[i];
+        //     if(count == 1) return nums[i];
         // }
+        // return 0;
+        //===============APPROACH 1=================
+        int xor = 0;
+        int n = nums.length;
+        for(int i = 0 ; i < n ; i++){
+            xor = xor ^ nums[i];
+        }
 
-        // return xor;
+        return xor;
     }
 }
