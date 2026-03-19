3d40d1b3d74d230d4da7effbe26a3cfea8fa63c8
Rudraksh Kushwaha <113119934+rudrakushwaha@users.noreply.github.com>
2026-03-19 23:33:52 +0530

    Time: 4 ms (62.01%) | Memory: 46.2 MB (35.48%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,37 +1,50 @@
 class Solution {
     public String removeOccurrences(String s, String part) {
 
-        int partLen = part.length();
-        int n = 0;
 
-        while(true){
+        //optimnal approach using stringbuilder:
+        StringBuilder sb = new StringBuilder();
+        int k = part.length();
 
-            n = s.length();
-            int index = findOccurence(s , part , n, partLen);
+        for( char ch: s.toCharArray()){
 
-            if(index == -1) break;
+            sb.append(ch);
 
-            // if ( (index + partLen) == n) {
-
-            //     s = s.substring(0, index);
-            //     break;
-
-            // } 
-            s = s.substring(0, index) + s.substring(index + partLen , n);
-
+            if(sb.length() >= k){
+                //last characters of sb if matching with the part, then we can delete that 
+                if(sb.substring(sb.length() - k).equals(part)){
+                    sb.delete(sb.length() - k, sb.length());
+                }
+            }
         }
+        return sb.toString();
 
-        return s;
+
+        // int partLen = part.length();
+        // int n = 0;
+
+        // while(true){
+
+        //     n = s.length();
+        //     int index = findOccurence(s , part , n, partLen);
+
+        //     if(index == -1) break;
+
+        //     s = s.substring(0, index) + s.substring(index + partLen , n);
+
+        // }
+
+        // return s;
     }
 
-    int findOccurence(String s, String part, int n , int partlen){
+    // int findOccurence(String s, String part, int n , int partlen){
 
-        for( int i = 0 ; i <= n - partlen ; i++){
+    //     for( int i = 0 ; i <= n - partlen ; i++){
 
-            if( s.substring(i, i + partlen).equals(part))
-            return i;
-        }
+    //         if( s.substring(i, i + partlen).equals(part))
+    //         return i;
+    //     }
 
-        return -1;
-    }
+    //     return -1;
+    // }
 }
