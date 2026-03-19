2158403d18d573ff7beda3b0ec0a643d3d843156
Rudraksh Kushwaha <113119934+rudrakushwaha@users.noreply.github.com>
2026-03-19 20:44:06 +0530

    Time: 2 ms (99.57%) | Memory: 44.5 MB (64.28%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,23 +3,44 @@ class Solution {
 
         HashMap<Character , Integer> map = new HashMap<>();
 
-        for(char ch: s.toCharArray()){
-            map.put(ch, map.getOrDefault(ch, 0 ) + 1);
-        }
 
+        ////////////////////==============using frequency map==
+        int[] freq = new int[26];
+
+        for(char ch: s.toCharArray()){
+            freq[ch - 'a']++;
+        }
 
         for(char ch: t.toCharArray()){
-
-             if (!map.containsKey(ch)) return false;
-            map.put(ch, map.get(ch) - 1);
-
-            if(map.get(ch) == 0)
-            map.remove(ch);
-
-            
+            freq[ch - 'a']--;
         }
 
-        return map.isEmpty();
+        for(int n: freq){
+            if( n != 0) return false;
+        }
+
+        return true;
+        ///////using hashmap 
+        // for(char ch: s.toCharArray()){
+        //     map.put(ch, map.getOrDefault(ch, 0 ) + 1);
+        // }
+
+
+        // for(char ch: t.toCharArray()){
+
+        //      if (!map.containsKey(ch)) return false;
+        //     map.put(ch, map.get(ch) - 1);
+
+        //     if(map.get(ch) == 0)
+        //     map.remove(ch);
+
+            
+        // }
+
+        // return map.isEmpty();
+
+
+
     }
 }
            
