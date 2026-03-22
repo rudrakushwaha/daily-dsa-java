275ff6581cf883053d6989108a8ba948ecdbb8e7
Rudraksh Kushwaha <113119934+rudrakushwaha@users.noreply.github.com>
2026-03-22 12:29:38 +0530

    Time: 25 ms (15.42%) | Memory: 53.6 MB (7.56%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,29 +1,29 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
 
-        //================APPROACH 3======================
+        //================APPROACH 3 (optimal)======================
         //algo which count sum <= k 
-        return subArrCount(nums, goal) - subArrCount(nums, goal - 1);
+        // return subArrCount(nums, goal) - subArrCount(nums, goal - 1);
 
         //================APPROACH 2 (similar to longest sub with sum k and count subarray with sum k)=================
-        //  int count = 0;
-        // HashMap<Long, Integer> map = new HashMap<>();
-        // long sum = 0;
-        // long remaining = 0;
-        // map.put(0L,1);
+         int count = 0;
+        HashMap<Long, Integer> map = new HashMap<>();
+        long sum = 0;
+        long remaining = 0;
+        map.put(0L,1);
 
-        // for( int i = 0 ; i < nums.length ; i++){
+        for( int i = 0 ; i < nums.length ; i++){
 
-        //     sum += nums[i];
-        //     remaining = sum - goal;
+            sum += nums[i];
+            remaining = sum - goal;
 
-        //     if( map.containsKey(remaining)){
-        //        count += map.get(remaining);
-        //     }
-        //      map.put(sum, map.getOrDefault(sum, 0) + 1);
+            if( map.containsKey(remaining)){
+               count += map.get(remaining);
+            }
+             map.put(sum, map.getOrDefault(sum, 0) + 1);
 
-        // }
-		// return count;
+        }
+		return count;
         
         //==================APPROACH 1==================
         // int count = 0;
@@ -42,29 +42,29 @@ class Solution {
         // return count;
     }
 
-    public int subArrCount(int[] nums , int goal) {
+    // public int subArrCount(int[] nums , int goal) {
 
-        if ( goal < 0) return 0;
+    //     if ( goal < 0) return 0;
 
-        int l = 0, r = 0;
-        int count = 0;
-        int n = nums.length;
-        int sum = 0;
+    //     int l = 0, r = 0;
+    //     int count = 0;
+    //     int n = nums.length;
+    //     int sum = 0;
 
-        while( r < n){
+    //     while( r < n){
 
-            sum += nums[r];
+    //         sum += nums[r];
 
-            while( sum > goal){
-                sum -= nums[l];
-                l += 1;
-            }
-            count += (r - l + 1);
+    //         while( sum > goal){
+    //             sum -= nums[l];
+    //             l += 1;
+    //         }
+    //         count += (r - l + 1);
 
-            r++;
+    //         r++;
 
-        }
+    //     }
 
-        return count;
-    }
+    //     return count;
+    // }
 }
