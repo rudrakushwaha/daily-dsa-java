6d7c591b6be571c5378bc12ca26180fab7d69b6f
Rudraksh Kushwaha <113119934+rudrakushwaha@users.noreply.github.com>
2026-06-22 03:26:40 +0530

    Time: 1 ms (99.89%) | Memory: 55.8 MB (17.47%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,18 +1,41 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // int max = nums.length / 2;
+
+        // for(int num : nums){
+        //     map.put(num, map.getOrDefault(num, 0) + 1);
+        // }
+
+        // for(int key: map.keySet()){
+        //     if(map.get(key) > max) 
+        //     return key;
+        // }
+
+        // return -1;
+
+
+        // ===============OPTIMAL(MOORE'S VOTING) ALGO==========
+        int currElem = 0;
+        int count = 0;
         int max = nums.length / 2;
 
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        for(int i = 0 ; i < nums.length ; i++){
+            
+            if(count == 0){
+                count = 1;
+                currElem = nums[i];
+            } else if( currElem == nums[i]){
+                count++;
+            } else {
+                count--;
+            }
         }
+        return currElem;
 
-        for(int key: map.keySet()){
-            if(map.get(key) > max) 
-            return key;
-        }
-
-        return -1;
+        // for( int i = 0 ; i < nums.length ; i++){
+        //     if(curr)
+        // }
     }
 }
