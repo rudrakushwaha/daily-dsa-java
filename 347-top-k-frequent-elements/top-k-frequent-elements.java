d7cf02231b26bd3ba149c578a91b0174c58775fe
Rudraksh Kushwaha <113119934+rudrakushwaha@users.noreply.github.com>
2026-07-18 14:33:01 +0530

    Time: 14 ms (70.84%) | Memory: 47.7 MB (48.72%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,34 +1,61 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int[] result = new int[k];
+        // ===========PRIORITY QUEUE====
+         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int num: nums){
-            map.put(num, map.getOrDefault(num,0)+1);
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        
-        for(int i = 0 ; i < k ; i++){
 
-            int maxFreq = -1;
-            int element = -1;
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a,b) -> a.getValue() - b.getValue());
 
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
 
-            for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            pq.offer(entry);
 
-                if(entry.getValue() > maxFreq){
-                    maxFreq = entry.getValue();
-                    element = entry.getKey();
-                }
-
-
+            if(pq.size() > k){
+                pq.poll();
             }
-
-            result[i] = element;
-            map.remove(element);
         }
 
+        int [] result = new int[k];
+
+        for(int i = k - 1 ; i >= 0; i--){
+            result[i] = pq.poll().getKey();
+        }
 
         return result;
+
+        //=================BRUTE FORCE APPROACH===
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // int[] result = new int[k];
+
+        // for(int num: nums){
+        //     map.put(num, map.getOrDefault(num,0)+1);
+        // }
+        
+        // for(int i = 0 ; i < k ; i++){
+
+        //     int maxFreq = -1;
+        //     int element = -1;
+
+
+        //     for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+
+        //         if(entry.getValue() > maxFreq){
+        //             maxFreq = entry.getValue();
+        //             element = entry.getKey();
+        //         }
+
+
+        //     }
+
+        //     result[i] = element;
+        //     map.remove(element);
+        // }
+
+
+        // return result;
     }
 }
