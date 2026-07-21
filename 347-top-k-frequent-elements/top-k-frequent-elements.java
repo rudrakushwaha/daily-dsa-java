fbe414e6f3c196c039b1d19bde8c78538d714309
Rudraksh Kushwaha <113119934+rudrakushwaha@users.noreply.github.com>
2026-07-21 17:26:01 +0530

    Time: 13 ms (79.97%) | Memory: 47.5 MB (82.13%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -2,13 +2,38 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
         // ===========PRIORITY QUEUE====
-         HashMap<Integer, Integer> map = new HashMap<>();
+        //  HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        // for (int num : nums) {
+        //     map.put(num, map.getOrDefault(num, 0) + 1);
+        // }
+
+        // PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.getValue(), b.getValue()));
+
+        // for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+
+        //     pq.offer(entry);
+
+        //     if(pq.size() > k){
+        //         pq.poll();
+        //     }
+        // }
+
+        // int [] result = new int[k];
+
+        // for(int i = 0 ; i < k; i++){
+        //     result[i] = pq.poll().getKey();
+        // }
+
+        // return result;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int num: nums){
+            map.put(num , map.getOrDefault(num,0) + 1);
         }
 
-        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a,b) -> Integer.compare(a.getValue(), b.getValue()));
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a,b)-> Integer.compare(a.getValue(), b.getValue()) );
 
         for(Map.Entry<Integer, Integer> entry: map.entrySet()){
 
@@ -17,16 +42,15 @@ class Solution {
             if(pq.size() > k){
                 pq.poll();
             }
+
         }
 
-        int [] result = new int[k];
-
-        for(int i = 0 ; i < k; i++){
+        int[] result = new int[k];
+        for(int i = 0 ; i < k; i++ ){
             result[i] = pq.poll().getKey();
         }
 
         return result;
-
         //=================BRUTE FORCE APPROACH===
         // HashMap<Integer, Integer> map = new HashMap<>();
         // int[] result = new int[k];
