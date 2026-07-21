63952e12c4cf86da817f53a2c621b3dfe65d8869
Rudraksh Kushwaha <113119934+rudrakushwaha@users.noreply.github.com>
2026-07-21 18:09:59 +0530

    Time: 30 ms (75.44%) | Memory: 95.7 MB (27.82%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -3,31 +3,61 @@ import java.util.*;
 class Solution {
     public int longestConsecutive(int[] nums) {
 
-        if (nums.length == 0) return 0;
+        // if (nums.length == 0) return 0;
 
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
 
-        int longest = 1;
-        int current = 1;
+        // int longest = 1;
+        // int current = 1;
 
-        for (int i = 1; i < nums.length; i++) {
+        // for (int i = 1; i < nums.length; i++) {
 
-            // Duplicate
-            if (nums[i] == nums[i - 1]) {
-                continue;
+        //     // Duplicate
+        //     if (nums[i] == nums[i - 1]) {
+        //         continue;
+        //     }
+
+        //     // Consecutive
+        //     else if (nums[i] == nums[i - 1] + 1) {
+        //         current++;
+        //     }
+
+        //     // Gap
+        //     else {
+        //         current = 1;
+        //     }
+
+        //     longest = Math.max(longest, current);
+        // }
+
+        // return longest;
+
+        //=======================optimal hashset solution================
+
+        HashSet<Integer> set = new HashSet<>();
+        int longest = 0;
+
+        for(int num : nums){
+            set.add(num);
+        }
+
+        for(int num : set){
+
+            //checking if the current number may be the start of the sequence
+            if(!set.contains(num - 1)) { //if number previous to it doesn't exist -> can be the start
+
+                int length = 1;
+                int current = num;
+
+                while( set.contains(current + 1)){
+                    current++;
+                    length++;
+                }
+            
+
+             longest = Math.max(longest, length);
             }
 
-            // Consecutive
-            else if (nums[i] == nums[i - 1] + 1) {
-                current++;
-            }
-
-            // Gap
-            else {
-                current = 1;
-            }
-
-            longest = Math.max(longest, current);
         }
 
         return longest;
