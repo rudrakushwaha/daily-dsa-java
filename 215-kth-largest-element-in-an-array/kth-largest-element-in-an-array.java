71e7584da38fdcfbb25bce96b2907ae0045324eb
Rudraksh Kushwaha <113119934+rudrakushwaha@users.noreply.github.com>
2026-08-04 18:19:03 +0530

    Time: 156 ms (6.90%) | Memory: 72.8 MB (92.32%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,40 +1,57 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-       int targetIdx = nums.length - k;
-        return quickSelect(nums, 0, nums.length - 1, targetIdx);
+
+
+    //    int targetIdx = nums.length - k;
+    //     return quickSelect(nums, 0, nums.length - 1, targetIdx);
+    PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+    for(int num : nums){
+        pq.offer(num);
+    }
+
+    while(k - 1 > 0){
+        pq.poll();
+        k--;
+    }
+
+    return pq.peek();
+
+
+
     }
         
-    private int quickSelect(int[] nums, int left, int right, int targetIdx) {
-        if (left == right) {
-            return nums[left];
-        }
+    // private int quickSelect(int[] nums, int left, int right, int targetIdx) {
+    //     if (left == right) {
+    //         return nums[left];
+    //     }
 
-        int pivot = nums[left];
-        int low = left;
-        int high = right;
+    //     int pivot = nums[left];
+    //     int low = left;
+    //     int high = right;
 
-        while (low <= high) {
-            while (low <= high && nums[low] < pivot) {
-                low++;
-            }
-            while (low <= high && nums[high] > pivot) {
-                high--;
-            }
-            if (low <= high) {
-                int temp = nums[low];
-                nums[low] = nums[high];
-                nums[high] = temp;
-                low++;
-                high--;
-            }
-        }
+    //     while (low <= high) {
+    //         while (low <= high && nums[low] < pivot) {
+    //             low++;
+    //         }
+    //         while (low <= high && nums[high] > pivot) {
+    //             high--;
+    //         }
+    //         if (low <= high) {
+    //             int temp = nums[low];
+    //             nums[low] = nums[high];
+    //             nums[high] = temp;
+    //             low++;
+    //             high--;
+    //         }
+    //     }
 
-        if (targetIdx <= high) {
-            return quickSelect(nums, left, high, targetIdx);
-        } else if (targetIdx >= low) {
-            return quickSelect(nums, low, right, targetIdx);
-        } else {
-            return nums[targetIdx];
-        }
-    }
+    //     if (targetIdx <= high) {
+    //         return quickSelect(nums, left, high, targetIdx);
+    //     } else if (targetIdx >= low) {
+    //         return quickSelect(nums, low, right, targetIdx);
+    //     } else {
+    //         return nums[targetIdx];
+    //     }
+    // }
 }
