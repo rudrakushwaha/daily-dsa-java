a21945c4bb9f9e548932ab4bb4e2c61f4af547a5
Rudraksh Kushwaha <113119934+rudrakushwaha@users.noreply.github.com>
2026-08-05 11:13:38 +0530

    Time: 2396 ms (5.37%) | Memory: 80.6 MB (5.02%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,9 +1,14 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
 
-
+        //optimal approach using quick sort
        int targetIdx = nums.length - k;
+
+
         return quickSelect(nums, 0, nums.length - 1, targetIdx);
+
+
+    ////////////////BETTER APPROACH WITH O(nlogk)
     // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
     // for(int num : nums){
@@ -21,37 +26,52 @@ class Solution {
 
     }
         
-    private int quickSelect(int[] nums, int left, int right, int targetIdx) {
-        if (left == right) {
-            return nums[left];
+    private int quickSelect(int[] nums, int low, int high, int targetIdx) {
+        
+        int pIndex = partitionIndex(nums, low, high, targetIdx);
+
+        if(pIndex == targetIdx){
+            return nums[pIndex];
         }
 
-        int pivot = nums[left];
-        int low = left;
-        int high = right;
-
-        while (low <= high) {
-            while (low <= high && nums[low] < pivot) {
-                low++;
-            }
-            while (low <= high && nums[high] > pivot) {
-                high--;
-            }
-            if (low <= high) {
-                int temp = nums[low];
-                nums[low] = nums[high];
-                nums[high] = temp;
-                low++;
-                high--;
-            }
-        }
-
-        if (targetIdx <= high) {
-            return quickSelect(nums, left, high, targetIdx);
-        } else if (targetIdx >= low) {
-            return quickSelect(nums, low, right, targetIdx);
-        } else {
-            return nums[targetIdx];
-        }
+        if(pIndex > targetIdx){
+            //sort left
+           return quickSelect(nums, low, pIndex-1, targetIdx);
+        } 
+   
+        return quickSelect(nums, pIndex + 1, high, targetIdx);
     }
+
+    public int partitionIndex(int[] nums, int low, int high, int targetIdx) {
+
+        int pivot = nums[low];
+        int i = low; 
+        int j = high;
+
+        while( i < j){
+
+            while( i < high && nums[i] <= pivot){
+                i++;
+            }
+
+            while( j > low && nums[j] > pivot){
+                j --;
+            }
+
+            if(i < j){
+                swap(nums, i, j);
+            }
+        }
+        swap(nums, low, j);
+        return j;
+
+    }
+
+    public void swap(int[] nums, int i, int j){
+
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+        
 }
